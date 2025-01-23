42e0fa105e8670b6fac0d9a8d633d829931d0142
singhranjitgit <singhranjitgit@users.noreply.github.com>
2025-01-23 15:50:17 +0530

    Rnj add services

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
-import { AboutComponent } from '../components/about/about.component';
 import { ContactComponent } from '../components/contact/contact.component';
 import { WorkComponent } from '../components/work/work.component';
 import { LogregComponent } from '../components/log-reg/log-reg.component';
 import { BlogsComponent } from '../components/blogs/blogs.component';
 import { BlogComponent } from '../components/blog/blog.component';
 import { FaqComponent } from '../components/faqs/faq.component';
+import { ServicesComponent } from '../components/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },  
-  { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'work', component: WorkComponent },
   { path: 'logreg', component: LogregComponent },
   { path: 'faq', component: FaqComponent },
+  { path: 'services', component: ServicesComponent },
   { path: 'blogs', 
     children: [
       {path: '', component: BlogsComponent},
--- src/app/services/work.service.ts
+++ src/app/services/work.service.ts
@@ -12,28 +12,28 @@ export class WorkService {
   getProjects(): any[]{
     return [
         {
-            background: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),url("../../../assets/image/Screenshot 2025-01-09 181027.webp")',
+            background: '../../../assets/image/Screenshot 2025-01-09 181027.webp',
             name: 'TvStartup',
             desc: 'TvStartup Inc. is a leading service provider in developing and deploying Internet Television media systems across all available platforms worldwide.',
             link: 'https://tvstartup.com/',
             btnTxt: 'Visit Project'
         },
         {
-            background: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),url("../../../assets/image/lpl.webp")',
+            background: '../../../assets/image/lpl.webp',
             name: 'letsplaylive',
             desc: 'Welcome to letsplaylive, the home of esports and gaming. Delivering the ultimate competitive gaming experience, from casual to pro.',
             link: 'https://gg.letsplay.live/home',
             btnTxt: 'Visit Project'
         },
         {
-            background: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),url("../../../assets/image/googly.webp")',
+            background: '../../../assets/image/googly.webp',
             name: 'Googly',
             desc: 'Googly, a part of Dynasty Gaming & Media, is a competitive gaming platform designed for India’s 500 million+ gamers.',
             link: 'https://platform.googly.gg/home',
             btnTxt: 'Visit Project'
         },
         {
-            background: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),url("../../../assets/image/lightningdragon.webp")',
+            background: '../../../assets/image/lightningdragon.webp',
             name: 'Lightning Dragon',
             desc: 'Welcome to Lightning Dragon, the home of esports and gaming for Philippines. Delivering the ultimate competitive gaming experience, from casual to pro.',
             link: 'https://lightningdragon.gg/',
